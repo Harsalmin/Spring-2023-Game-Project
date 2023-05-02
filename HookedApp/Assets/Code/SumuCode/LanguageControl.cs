9a8d85c246bf452c290dc32309cdf48b61c1f91b
Sumu Vuori <sumu.vuori@tuni.fi>
2023-05-02 16:39:42 +0300

    Language changes

--- HookedApp/Assets/Code/SumuCode/LanguageControl.cs
+++ HookedApp/Assets/Code/SumuCode/LanguageControl.cs
@@ -8,6 +8,8 @@ public class LanguageControl : MonoBehaviour
     [SerializeField] TMP_Text chat, events, logs, settings, logsTitle;
     [SerializeField] TMP_Text settingsTitle, settingsDesc, save, load, exit;
     [SerializeField] TMP_Text eventYes, eventNo, eventPopup;
+    [SerializeField] TMP_Text[] eventTitles;
+    [SerializeField] TMP_Text grandmaConvo, grandmaButton, doormanConvo, doormanButton, gymManConvo, gymManButton;
 
     // Start is called before the first frame update
     void Start()
@@ -41,6 +43,30 @@ public class LanguageControl : MonoBehaviour
         exit.text = "Takaisin päävalikkoon";
         eventYes.text = "Osallistun";
         eventNo.text = "En osallistu";
+
+        eventTitles[0].text = "Baari-ilta";
+        eventTitles[1].text = "Työmessut";
+        eventTitles[2].text = "Klubikeikka";
+        eventTitles[3].text = "DJ-keikka";
+        eventTitles[4].text = "Rekrymessut";
+        eventTitles[5].text = "DJ-keikka";
+        eventTitles[6].text = "Työmessut";
+        eventTitles[7].text = "Pelikahvila";
+        eventTitles[8].text = "Pelibaari";
+        eventTitles[9].text = "Kahvila";
+        eventTitles[10].text = "Baari-ilta";
+        eventTitles[11].text = "Kuntosali";
+        eventTitles[12].text = "Sulkapallo";
+        eventTitles[13].text = "Nuorisotalo";
+        eventTitles[14].text = "Opiskelijatapahtuma";
+        eventTitles[15].text = "Pelimessut";
+
+        grandmaConvo.text = "Eila-mummo";
+        grandmaButton.text = "Eila-mummo";
+        doormanConvo.text = "Joni, portsari";
+        doormanButton.text = "Joni, portsari";
+        gymManButton.text = "Kari salilta";
+        gymManConvo.text = "Kari salilta";
     }
 
     public void English()
@@ -57,5 +83,29 @@ public class LanguageControl : MonoBehaviour
         exit.text = "Back to main menu";
         eventYes.text = "Attending";
         eventNo.text = "Not attending";
+
+        eventTitles[0].text = "Bar night";
+        eventTitles[1].text = "Work fair";
+        eventTitles[2].text = "Club gig";
+        eventTitles[3].text = "DJ gig";
+        eventTitles[4].text = "Recruiting fair";
+        eventTitles[5].text = "DJ gig";
+        eventTitles[6].text = "Work fair";
+        eventTitles[7].text = "Gaming café";
+        eventTitles[8].text = "Game bar";
+        eventTitles[9].text = "Café";
+        eventTitles[10].text = "Bar night";
+        eventTitles[11].text = "Gym";
+        eventTitles[12].text = "Badminton";
+        eventTitles[13].text = "Youth centre";
+        eventTitles[14].text = "Student event";
+        eventTitles[15].text = "Game fair";
+
+        grandmaConvo.text = "Grandma Eila";
+        grandmaButton.text = "Grandma Eila";
+        doormanConvo.text = "Joni, doorman";
+        doormanButton.text = "Joni, doorman";
+        gymManButton.text = "Kari from the gym";
+        gymManConvo.text = "Kari from the gym";
     }
 }
